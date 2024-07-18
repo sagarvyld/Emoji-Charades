e0420bc7a273b77f9f525838f0e8982234bb83e3
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-07-18 17:54:39 +0530

    emoji page

--- src/components/BottomSlidebar.jsx
+++ src/components/BottomSlidebar.jsx
@@ -16,10 +16,11 @@ const BottomSlidebar = ({ isOpen, onClose, onSelectTopic, selectedTopic }) => {
                         key={index}
                         className={`topicItem ${selectedTopic === topic ? 'selected' : ''}`}
                         onClick={() => onSelectTopic(topic)}
-                    > <span className='topicItemtxt' style={{ color: selectedTopic === topic ? '#C6FF00' : '' }}
-                    >{topic}</span>
+                    >
+                        <span className='topicItemtxt' style={{ color: selectedTopic === topic ? '#C6FF00' : '' }}
+                        >{topic}</span>
                         {selectedTopic === topic && (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                            <svg style={{ position:"absolute", right:"1.5rem"}}xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                                 <circle cx="9" cy="9" r="8.5" fill="#C6FF00" stroke="#161716" />
                                 <path d="M5.14282 9.00002L7.71425 11.5714L12.8571 6.42859" stroke="#161716" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
